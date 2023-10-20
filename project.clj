370976b60600b9b2f0ca5950771ac04c1668aa5d
skylee03 <1178715749@qq.com>
2023-10-20 13:41:03 +0800

    Upgrade Clojask to v2.0.2

--- project.clj
+++ project.clj
@@ -6,16 +6,17 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [techascent/tech.ml.dataset "7.014"]
                  [scicloj/tablecloth "7.014"]
-                 [com.github.clojure-finance/clojask "2.0.0"]
+                 [com.github.clojure-finance/clojask "2.0.2"]
                  [zero.one/geni "0.0.40"]
                  [com.fasterxml.jackson.core/jackson-core "2.15.2"]
                  [metrics-clojure "2.10.0"]
-                 [org.apache.poi/poi "5.2.3"]]
+                 [org.apache.poi/poi "5.2.3"]
+                 [org.apache.zookeeper/zookeeper "3.7.2" :exclusions [org.slf4j/slf4j-log4j12]]]
   :main ^:skip-aot datajure.dsl
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
-             :provided {:dependencies [[org.apache.spark/spark-core_2.12 "3.1.1"]
+             :provided {:dependencies [[org.apache.spark/spark-core_2.12 "3.1.1" :exclusions [org.slf4j/slf4j-log4j12]]
                                        [org.apache.spark/spark-mllib_2.12 "3.1.1"]
                                        [org.apache.spark/spark-sql_2.12 "3.1.1"]
                                        [org.apache.arrow/arrow-vector "2.0.0"
