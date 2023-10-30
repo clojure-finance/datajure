745aa02d122d192fb55acd221fdf4be6940deb57
skylee03 <1178715749@qq.com>
2023-10-30 14:11:12 +0800

    Include group name in `defproject`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datajure "1.0.0"
+(defproject com.github.clojure-finance/datajure "1.0.0"
   :description "An open-source domain-specific language for data processing."
   :url "https://clojure-finance.github.io/datajure-website/"
   :license {:name "The MIT License"
