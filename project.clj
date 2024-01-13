f5e360ee561a5cdcff5f58192bb648f8448a7469
Yang Ming-Tian <1178715749@qq.com>
2024-01-13 18:51:46 +0800

    Update dependencies to fix #43 (#44)
    * Update dependencies to fix #43

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "https://clojure-finance.github.io/datajure-website/"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[log4j/log4j "1.2.17"]
+                 [org.apache.logging.log4j/log4j-core "2.21.0"]
+                 [com.fasterxml.jackson.core/jackson-core "2.15.3"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.15.3"]
+                 [org.clojure/clojure "1.11.1"]
                  [techascent/tech.ml.dataset "7.020"]
                  [scicloj/tablecloth "7.014"]
                  [com.github.clojure-finance/clojask "2.0.2"]
@@ -11,7 +15,16 @@
                  [com.fasterxml.jackson.core/jackson-core "2.15.3"]
                  [metrics-clojure "2.10.0"]
                  [org.apache.poi/poi "5.2.4"]
-                 [org.apache.zookeeper/zookeeper "3.7.2" :exclusions [org.slf4j/slf4j-log4j12]]]
+                 [org.apache.zookeeper/zookeeper "3.7.2" :exclusions [org.slf4j/slf4j-log4j12]]
+                 [org.apache.arrow/arrow-memory-core "4.0.0"]
+                 [org.apache.arrow/arrow-vector "4.0.0"
+                  :exclusions [commons-codec com.fasterxml.jackson.core/jackson-databind]]
+                 [org.apache.spark/spark-avro_2.12 "3.3.3"]
+                 [org.apache.spark/spark-core_2.12 "3.3.3" :exclusions [org.apache.logging.log4j/log4j-slf4j-impl]]
+                 [org.apache.spark/spark-hive_2.12 "3.3.3"]
+                 [org.apache.spark/spark-mllib_2.12 "3.3.3"]
+                 [org.apache.spark/spark-sql_2.12 "3.3.3"]
+                 [org.apache.spark/spark-streaming_2.12 "3.3.3"]]
   :main ^:skip-aot datajure.dsl
   :target-path "target/%s"
   :jvm-opts ["--add-opens=java.base/java.nio=ALL-UNNAMED"
@@ -23,26 +36,6 @@
   :profiles {:uberjar {:aot :all
                        :plugins [[arctype/log4j2-plugins-cache "1.0.0"]]
                        :middleware [leiningen.log4j2-plugins-cache/middleware]
-                       :manifest {"Multi-Release" true}
-                       :dependencies [[log4j/log4j "1.2.17"]
-                                      [org.apache.logging.log4j/log4j-core "2.21.0"]
-                                      ;; Arrow
-                                      [org.apache.arrow/arrow-memory-core "4.0.0"]
-                                      [org.apache.arrow/arrow-vector "4.0.0"
-                                       :exclusions [commons-codec com.fasterxml.jackson.core/jackson-databind]]
-                                      ;; Spark
-                                      [org.apache.spark/spark-avro_2.12 "3.3.3"]
-                                      [org.apache.spark/spark-core_2.12 "3.3.3"]
-                                      [org.apache.spark/spark-hive_2.12 "3.3.3"]
-                                      [org.apache.spark/spark-mllib_2.12 "3.3.3"]
-                                      [org.apache.spark/spark-sql_2.12 "3.3.3"]
-                                      [org.apache.spark/spark-streaming_2.12 "3.3.3"]]}
+                       :manifest {"Multi-Release" true}}
              :test {:dependencies [[org.apache.logging.log4j/log4j-core "2.21.0"]]}
-             :repl {:dependencies [[org.apache.logging.log4j/log4j-core "2.21.0"]]}
-             :provided {:dependencies [[com.fasterxml.jackson.core/jackson-core "2.15.3"]
-                                       [com.fasterxml.jackson.core/jackson-annotations "2.15.3"]
-                                       [org.apache.spark/spark-core_2.12 "3.3.3" :exclusions [org.apache.logging.log4j/log4j-slf4j-impl]]
-                                       [org.apache.spark/spark-mllib_2.12 "3.3.3"]
-                                       [org.apache.spark/spark-sql_2.12 "3.3.3"]
-                                       [org.apache.arrow/arrow-vector "4.0.0"
-                                        :exclusions [commons-codec com.fasterxml.jackson.core/jackson-databind]]]}})
+             :repl {:dependencies [[org.apache.logging.log4j/log4j-core "2.21.0"]]}})
