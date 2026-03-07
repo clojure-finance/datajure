c4fa4e02f1734cac38e49a452798b0058b9685be
Matt Buehlmaier <buehlmaier@users.noreply.github.com>
2026-03-07 18:00:48 +0800

    Fix CI: move test command to shell script to avoid YAML quoting issues

---
+++ bin/run-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+clj -Sdeps '{:paths ["src" "resources" "test"] :deps {org.clojure/clojure {:mvn/version "1.12.4"} techascent/tech.ml.dataset {:mvn/version "8.003"}}}' \
+  -M -e "
+(require '[clojure.test :as t])
+(load-file \"test/datajure/core_test.clj\")
+(load-file \"test/datajure/concise_test.clj\")
+(load-file \"test/datajure/util_test.clj\")
+(load-file \"test/datajure/io_test.clj\")
+(load-file \"test/datajure/reshape_test.clj\")
+(load-file \"test/datajure/join_test.clj\")
+(let [result (t/run-tests
+               'datajure.core-test 'datajure.concise-test
+               'datajure.util-test 'datajure.io-test
+               'datajure.reshape-test 'datajure.join-test)]
+  (System/exit (+ (:fail result) (:error result))))"
