c17e585c7825f507aa1ba034c554b06759c43aaf
Matt Buehlmaier <buehlmaier@users.noreply.github.com>
2026-07-19 10:53:05 +0800

    Bump tech.ml.dataset from 8.007 to 8.024
    Aligns with the org-wide TMD target. Bumped in both deps.edn and the
    inline -Sdeps pin in bin/run-tests.sh (the CI script pins TMD
    independently of deps.edn). Test suite identical before and after:
    401 tests, 1505 assertions, 0 failures.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-clojure -Sdeps '{:paths ["src" "resources" "test"] :deps {org.clojure/clojure {:mvn/version "1.12.4"} techascent/tech.ml.dataset {:mvn/version "8.007"}}}' \
+clojure -Sdeps '{:paths ["src" "resources" "test"] :deps {org.clojure/clojure {:mvn/version "1.12.4"} techascent/tech.ml.dataset {:mvn/version "8.024"}}}' \
   -M -e "
 (require '[clojure.test :as t])
 (load-file \"test/datajure/core_test.clj\")
